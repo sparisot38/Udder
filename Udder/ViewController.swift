f130ac1af0e9c18804e9ea00426ba3e6c480b60d
stephaneparisot <stephane@wiplaymusic.com>
2020-06-12 11:56:55 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by sparisot on 12/06/2020.
+//  Copyright © 2020 Stéphane Parisot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
